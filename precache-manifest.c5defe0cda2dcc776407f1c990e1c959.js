36eea46cc2c855eb6a0d197c474ef6806d11cb7a
AvivAbachi <AvivAbachi@Gmail.com>
2020-09-03 20:03:19 +0300

    Updates

---
+++ precache-manifest.c5defe0cda2dcc776407f1c990e1c959.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fda49219f079132a6f940891f6e7e34f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f54426a9115ea4e8cc00",
+    "url": "/static/css/main.473ceb27.chunk.css"
+  },
+  {
+    "revision": "bac69eb1663885f20a91",
+    "url": "/static/js/2.a4e874d5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.a4e874d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f54426a9115ea4e8cc00",
+    "url": "/static/js/main.4b2a0929.chunk.js"
+  },
+  {
+    "revision": "4b79fa00ca29cf0bf24c",
+    "url": "/static/js/runtime-main.1baaf854.js"
+  }
+]);
